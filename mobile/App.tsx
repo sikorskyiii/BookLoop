f9634e7fcaae59fc359dcbf840e5b909346d986d
hi <hi@his-MacBook-Air.local>
2025-08-09 22:34:05 +0300

    refactor(mobile): wire Redux Provider + AppNavigator

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { AppNavigator } from './src/navigation/AppNavigator';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <NewAppScreen templateFileName="App.tsx" />
-      </View>
+      <AppNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
