bdefd1650935128b800bd8e67fee7b0a80c00377
sikorskyiii <off.sikorskyi@gmail.com>
2025-08-28 20:59:55 +0300

    feat(api): axios client with setAuthToken and Authorization header

--- apps/mobile/src/api/client.js
+++ apps/mobile/src/api/client.js
@@ -1,2 +1,10 @@
 import axios from "axios";
 export const api = axios.create({ baseURL: "http://localhost:3000" });
+let authToken = null;
+export function setAuthToken(t) {
+  authToken = t || null;
+}
+api.interceptors.request.use((config) => {
+  if (authToken) config.headers.Authorization = `Bearer ${authToken}`;
+  return config;
+});
