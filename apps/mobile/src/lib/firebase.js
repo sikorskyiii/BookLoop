13f275f407ebab396ada955c40aca465f4c1daa1
sikorskyiii <off.sikorskyi@gmail.com>
2025-09-04 22:11:46 +0300

    feat(firebase): init firebase app and export auth

--- apps/mobile/src/lib/firebase.js
+++ apps/mobile/src/lib/firebase.js
@@ -1,15 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import Constants from "expo-constants";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCtWbTEZqMUKo09N_5n1BPRMHKZCKrEW74",
-  authDomain: "bookloop-af1c8.firebaseapp.com",
-  projectId: "bookloop-af1c8",
-  storageBucket: "bookloop-af1c8.firebasestorage.app",
-  messagingSenderId: "921038747639",
-  appId: "1:921038747639:web:68c35241ad523cec38a0f2",
-  measurementId: "G-3HE42XHYFJ"
-};
+const firebaseConfig = Constants?.expoConfig?.extra?.firebase || {};
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
